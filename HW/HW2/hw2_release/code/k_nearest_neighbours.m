5c0d9981aaa1b15db1d8dad87e2dcf3df49ae764
Eric Oh <ericoh17@gmailcom>
2017-09-16 17:41:39 -0400

    first implementation of kNN

--- HW/HW2/hw2_release/code/k_nearest_neighbours.m
+++ HW/HW2/hw2_release/code/k_nearest_neighbours.m
@@ -16,4 +16,31 @@ function labels = k_nearest_neighbours(Xtrain,Ytrain,Xtest,K,distfunc)
     
     % YOUR CODE GOES HERE.
     
+    num_train = size(Xtrain, 1) ;
+    num_test = size(Xtest, 1) ;
+    
+    labels = zeros(num_test, 1) ;
+    
+    for test = 1:num_test
+        test_point = Xtest(test,:) ;
+        test_dists = zeros(num_train, 1) ; 
+        
+        if distfunc == "l1"
+            test_dist(test) = sum(abs(bsxfun(@minus, Xtrain, test_point)), 2) ;
+        elseif distfunc == "l2"
+            test_dist(test) = sqrt(sum(bsxfun(@minus, Xtrain, test_point).^2, 2)) ;
+        elseif distfunc == "linf"
+            test_dist(test) = max(abs(bsxfun(@minus, Xtrain, test_point)), [], 2) ;
+        end
+        
+        dist_sort = sort(test_dist) ; 
+        k_nn = dist_sort(1 : K) ; 
+    end
 end
+
+        
+        
+        
+        
+        
+        
