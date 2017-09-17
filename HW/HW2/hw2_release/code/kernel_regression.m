3b328378c4b6f986d27fc1644fc7a0a1752a8f4b
Eric Oh <ericoh17@gmailcom>
2017-09-16 21:41:20 -0400

    first implementation of kernel regression

--- HW/HW2/hw2_release/code/kernel_regression.m
+++ HW/HW2/hw2_release/code/kernel_regression.m
@@ -11,7 +11,22 @@ function labels = kernel_regression(Xtrain,Ytrain,Xtest,sigma)
     % sigma : width of the (gaussian) kernel.
     % labels : return an M x 1 vector of predicted labels for testing data.
     
-
+    Ytrain(Ytrain == 0) = -1 ; % set 0 to -1 for convenience
     
-    % YOUR CODE GOES HERE
+    num_test = size(Xtest, 1) ;
+    labels = zeros(num_test, 1) ;  % empty vector for labels
+    
+    scale = 1 / (2 * sigma^2) ; 
+    
+    for test = 1:num_test
+        test_point = Xtest(test,:) ;
+        
+        kern = exp(-scale * (bsxfun(@minus, Xtrain, test_point).^2)) ;
+        weight_kern = sum(kern .* y) ;
+        
+        if mode(sign(weight_kern)) == -1
+            labels(test) = 0 ;  % set label to 0 to switch back from -1
+        else 
+            labels(test) = 1 ;
+    end
 end
