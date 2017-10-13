902a8a5447e5c1ebf22a70830175e18df952a23d
Eric Oh <ericoh17@gmail.com>
2017-10-13 13:24:26 -0400

    get jerry's rbf and svm_train code

--- HW/HW4/hw4_release/SVM-problem/SVM_train.m
+++ HW/HW4/hw4_release/SVM-problem/SVM_train.m
@@ -1,4 +1,4 @@
-function model [pred_labels] = SVM_train(test_data, kerneltype)
+function [pred_labels] = SVM_train(test_data, kerneltype)
     % INPUT : 
     % test_data   - m X n matrix, where m is the number of test points and n is number of features
     % kerneltype  - one of strings 'poly', 'rbf'
@@ -11,15 +11,81 @@ function model [pred_labels] = SVM_train(test_data, kerneltype)
     
     % Default code below. Fill in your code on all the relevant positions
 
-    %m = size(test_data , 1);
-    %n = size(test_data, 2);
+    m = size(test_data , 1);
+    n = size(test_data, 2);
 
-    %load train_data
-
-    datadir = 'Breast-Cancer/';
-
-    load(strcat(datadir,'train.mat'));
     
+    %load cross-validation data
+    
+    C=[1,10,10^2,10^3,10^4,10^5];
+    Q=[1,2,3,4,5];
+    Sigma=[0.01,1,10,10^2,10^3];
+    
+    
+    if strcmpi(kerneltype, 'poly')
+        train_error = zeros(length(C),length(Q));
+    elseif strcmpi(kerneltype, 'rbf')
+        train_error = zeros(length(C),length(Sigma));
+    else
+        error('wrong kernel type');
+    end
+    
+    
+        for i=1:size(train_error,1)
+            for j=1:size(train_error,2)
+                errors = zeros(1,5);
+                for fold=1:5
+                    datadir=strcat('Breast-Cancer/CrossValidation/Fold',num2str(fold),'/cv-train.mat');
+                    load(datadir);
+                    train_data = cv_train;
+                    train_feature = train_data(:,1:(end-1));
+                    train_label = train_data(:,end);
+                    
+                    datadir=strcat('Breast-Cancer/CrossValidation/Fold',num2str(fold),'/cv-test.mat');
+                    load(datadir);
+                    test =cv_test;
+                    test_feature = test(:,1:(end-1));
+                    test_label = test(:,end);
+                    if strcmpi(kerneltype, 'poly')
+                        arg = horzcat('-t 1 -g 1 -r 1 -d ',num2str(Q(j)),' -c ',num2str(C(i)),' -q');
+                    else
+                        arg = horzcat('-t 2 -g ',num2str(Sigma(j)),' -c ',num2str(C(i)),' -q');
+                    end
+                    model = svmtrain(train_label, train_feature, arg);
+                    [~, accuracy, ~] = svmpredict(test_label, test_feature, model);
+                    errors(fold) = 1-accuracy(1)/100;
+                end
+                train_error(i,j) = mean(errors);
+            end
+        end
+        
+    A=min(train_error);
+    min_error = min(A);
+    [row,column]=find(train_error==min_error);
+    
+    datadir=strcat('Breast-Cancer/train.mat');
+    load(datadir);
+    train_data=train;
+    train_feature = train_data(:,1:(end-1));
+    train_label = train_data(:,end);
+    
+    C_opt=C(row);
+    if strcmpi(kerneltype, 'poly')
+        Q_opt = Q(column);
+        arg = horzcat('-t 1 -g 1 -r 1 -d ',num2str(Q_opt),' -c ',num2str(C_opt),' -q');
+    else
+        Sigma_opt = Sigma(column);
+        arg = horzcat('-t 2 -g ',num2str(Sigma_opt),' -c ',num2str(C_opt),' -q');
+    end
+    
+    model = svmtrain(train_label, train_feature, arg);
+    test_feature = test_data(:,1:(end-1));
+    test_label = test_data(:,end);
+    
+    [pred_labels, ~, ~] = svmpredict(test_label, test_feature, model);
+
+
+
     % Do cross-validation
     % For all c
     % For all kernel parameters
---
+++ HW/HW4/hw4_release/SVM-problem/svm_rbf.m
@@ -0,0 +1,86 @@
+addpath('C:\Users\Jerry Lu\Desktop\Upenn Study\Fall 2017\CIS 520\HW4\hw4_kit\hw4_release\SVM-problem\libsvm-3.22\libsvm-3.22\matlab')
+%kernel svm using rbf kernel:
+C=[1,10,10^2,10^3,10^4,10^5];
+Sigma=[0.01,1,10,10^2,10^3];
+
+
+
+C_opt = zeros(1,length(Sigma));
+
+for i=1:length(Sigma)
+    test_error = zeros(5,length(C)); %for each fold and each C
+ for fold=1:5
+        datadir=strcat('Synthetic/CrossValidation/Fold',num2str(fold),'/cv-train.mat');
+        train_data=load(datadir);
+        train_data = train_data.cv_train;
+        train_feature = train_data(:,1:(end-1));
+        train_label = train_data(:,end);
+        
+        datadir=strcat('Synthetic/CrossValidation/Fold',num2str(fold),'/cv-test.mat');
+        test_data=load(datadir);
+        test_data = test_data.cv_test;
+        test_feature = test_data(:,1:(end-1));
+        test_label = test_data(:,end);
+        
+        
+        for j=1:length(C)
+            arg = horzcat('-t 2 -g ',num2str(Sigma(i)),' -c ',num2str(C(j)),' -q');
+            model = svmtrain(train_label, train_feature, arg);
+            [predict, accuracy, dec_values] = svmpredict(test_label, test_feature, model);
+            test_error(fold,j) = 1-accuracy(1)/100;
+        end
+ end
+    mean_test_error = mean(test_error);
+    [~,index] = min(mean_test_error);
+    C_opt(i) = C(index);
+end
+
+train_error = zeros(1,length(Sigma));
+test_error = zeros(1,length(Sigma));
+ 
+for i=1:length(Sigma)
+    datadir=strcat('Synthetic/train.mat');
+    train_data=load(datadir);
+    train_data = train_data.train;
+    train_feature = train_data(:,1:(end-1));
+    train_label = train_data(:,end);
+    
+    datadir=strcat('Synthetic/test.mat');
+    test_data=load(datadir);
+    test_data = test_data.test;
+    test_feature = test_data(:,1:(end-1));
+    test_label = test_data(:,end);
+    
+    
+    arg = horzcat('-t 2 -g ',num2str(Sigma(i)),' -c ',num2str(C_opt(i)),' -q');
+    model = svmtrain(train_label, train_feature, arg);
+    [predict, accuracy, dec_values] = svmpredict(train_label, train_feature, model);
+    train_error(i) = 1-accuracy(1)/100;
+    
+    [predict, accuracy, dec_values] = svmpredict(test_label, test_feature, model);
+    test_error(i) = 1-accuracy(1)/100;
+    
+    %save the plot for decision boundary;
+    file = strcat('SVM_RBF_train_sigma_',num2str(Sigma(i)));
+    decision_boundary_SVM(train_feature, train_label, model, 200, file);
+    file = strcat('SVM_RBF_test_sigma_',num2str(Sigma(i)));
+    decision_boundary_SVM(test_feature, test_label, model, 200, file);
+end
+
+    figure;
+    hold on
+    plot(log10(Sigma),train_error,'b',log10(Sigma),test_error,'r');
+    legend('training error', 'testing error');
+    xlabel('log(sigma)');
+    ylabel('CV error');
+    hold off;
+
+    filename = strcat('Plots/CV.png');
+    title('CV Errors','FontSize', 16, 'FontWeight', 'bold');
+    %print(h, '-dpdf', filename);
+    saveas(gcf,filename,'png'); 
+
+
+[~,index] = min(test_error);
+Sigma_opt = Sigma(index);
+Sigma_opt
